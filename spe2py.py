ab4875111255953b1fd1bb942f81be06e047f0c5
ashirsch <alexander_hirsch@brown.edu>
2016-06-17 12:26:22 -0400

    Debugged to read_data and fixed header byte problems

--- spe2py.py
+++ spe2py.py
@@ -3,6 +3,7 @@
 spe2py imports a Princeton Instruments LightField (SPE 3.0) file into a python environment.
 """
 
+import struct
 import numpy as np
 import tkinter as tk
 import untangle
@@ -16,19 +17,29 @@ def get_file():
     return fname
 
 
-def read_header(filename):
-    f = open(filename, 'rb')
-    header = np.fromfile(f, np.uint8, 4100)
-    f.close()
-    return header
+def read_at(file, pos, size, ntype):
+    file.seek(pos)
+    return np.fromfile(file, ntype, size)
+
+
+# def read_header(filename):
+#     f = open(filename, 'rb')
+#     header_bytes = f.read(4100)
+#     header = np.empty(4100, np.uint8)
+#     L = [header_bytes[i:i+1] for i in range(len(header_bytes))]
+#     header = np.asarray([int.from_bytes(L[i], byteorder='big') for i in range(len(L))])
+#     f.close()
+#     return header
 
 
 def read_footer(filename):
     f = open(filename, 'rb')
-    f.seek(678)
-    footer_pos = np.fromfile(f, np.uint64, 8)[0]
-    f.seek(footer_pos)
+    # f.seek(678)
+    # footer_pos = np.fromfile(f, np.uint64, 8)[0]
+    # f.seek(footer_pos)
+    footer_pos = read_at(f, 678, 8, np.uint64)[0]
 
+    f.seek(footer_pos)
     xmlfile = open('xmlFile.tmp', 'w')
     xmltext = f.read().decode('utf-8')
 
@@ -42,7 +53,7 @@ def read_footer(filename):
     # TODO: print entire footer structure
 
 
-def get_specs(header, footer):
+def get_specs(filename, footer):
     cameraSettings = footer.SpeFormat.DataHistories.DataHistory.Origin.Experiment.Devices.Cameras.Camera
     regionOfInterest = cameraSettings.ReadoutControl.RegionsOfInterest.CustomRegions.RegionOfInterest
 
@@ -55,8 +66,14 @@ def get_specs(header, footer):
 
     wavelength = footer.SpeFormat.Calibrations.WavelengthMapping.Wavelength.cdata
 
-    nframes = header[1446:1447].astype(np.uint16)[0]
-    dtype_code = header[108:109].astype(np.uint16)[0]
+    # nframes = header[1446:1447].astype(np.uint16)[0]
+    # dtype_code = header[108:109].astype(np.uint16)[0]
+
+    f = open(filename, 'rb')
+    nframes = read_at(f, 1446, 2, np.uint16)[0]
+    dtype_code = read_at(f, 108, 2, np.uint16)[0]
+    xdim = read_at(f, 42, 2, np.uint16)[0]
+    ydim = read_at(f, 656, 2, np.uint16)[0]
 
     if dtype_code == 0:
         dtype = np.float32
@@ -69,10 +86,11 @@ def get_specs(header, footer):
     elif dtype_code == 8:
         dtype = np.uint32
 
-    return RoI, wavelength, nRoI, nframes, dtype
+    f.close()
+    return RoI, wavelength, nRoI, nframes, dtype, xdim, ydim
 
 
-def get_coords(header, RoI, nRoI):
+def get_coords(RoI, nRoI):
     xcoord = [[] for x in range(0, nRoI)]
     ycoord = [[] for x in range(0, nRoI)]
 
@@ -94,19 +112,26 @@ def get_coords(header, RoI, nRoI):
     return xcoord, ycoord
 
 
-def read_data(filename, dtype, nframes, nRoI, xcoord, ycoord):
-    data = np.empty([nframes, nRoI])
+def read_data(filename, dtype, nframes, nRoI, xcoord, ycoord, xdim, ydim):
+    # data = np.empty([nframes, nRoI])
     f = open(filename, 'rb')
     f.seek(4100)
 
-    xdim = len(xcoord)
-    ydim = len(ycoord)
+    # xdim = header[42:43].astype(np.uint16)[0]
+    # ydim = header[656:657].astype(np.uint16)[0]
 
     dataMatrix = [[0 for x in range(nRoI)] for y in range(nframes)]
     for frame in range(0, nframes):
         for RoI in range(0, nRoI):
+            if nRoI > 1:
+                xdim = len(xcoord[RoI])
+                ydim = len(ycoord[RoI])
+            else:
+                xdim = np.asarray(xdim, np.uint32)
+                ydim = np.asarray(ydim, np.uint32)
             dataMatrix[frame][RoI] = np.fromfile(f, dtype, xdim * ydim)
-            dataMatrix[frame][RoI] = np.reshape(data[frame, RoI], [ydim, xdim])
+            sz = dataMatrix[frame][RoI].size
+            dataMatrix[frame][RoI] = dataMatrix[frame][RoI].reshape(ydim, xdim)
     return dataMatrix
  # TODO: fix data writing
 
@@ -137,13 +162,17 @@ class SPE(object):
 
 def load():
     file = get_file()
-    loaded_header = read_header(file)
+
+    # loaded_header = read_header(file)
     loaded_footer = read_footer(file)
-    RoI, wavelength, nRoI, nframes, dtype = get_specs(loaded_header, loaded_footer)
+    RoI, wavelength, nRoI, nframes, dtype, xdim, ydim  = get_specs(file, loaded_footer)
+    xcoord, ycoord = get_coords(RoI, nRoI)
+
+    data = read_data(file, dtype, nframes, nRoI, xcoord, ycoord, xdim, ydim)
 
     # loaded_spe = SPE(file, loaded_header)
-
-    return loaded_header, loaded_footer, RoI, wavelength, nRoI, nframes, dtype
+    return data
+    #return loaded_header, loaded_footer, RoI, wavelength, nRoI, nframes, dtype, xcoord, ycoord, data
 
 
 def print_footer(footer, ind=-1):
